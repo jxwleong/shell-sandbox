1d687c88a5087c36c1239a3cb80d20385d889577
jxwleong <jason.xie.wei.leong@gmail.com>
2021-10-18 20:21:33 +0800

    Added shell script example to setup bashdb

---
+++ setup_bashdb.sh
@@ -0,0 +1,5 @@
+# Reference: https://unix.stackexchange.com/a/484609
+
+wget https://jaist.dl.sourceforge.net/project/bashdb/bashdb/5.0-1.1.2/bashdb-5.0-1.1.2.tar.gz
+tar -xf bashdb-5.0-1.1.2.tar.gz
+sudo cp bashdb-5.0-1.1.2  /usr/share/bashdb -r
